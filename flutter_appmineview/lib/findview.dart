785988403aeb0b77722aa01a1558c1219745e4fa
Bobo <HKJBobo@gmail.com>
2019-02-06 16:32:02 -0500

    merge to develop

--- flutter_appmineview/lib/findview.dart
+++ flutter_appmineview/lib/findview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'findview/question.dart';
 
 class findview extends StatefulWidget{
   @override
@@ -12,8 +13,78 @@ class findviewstate extends State<findview>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: Text('findview'),
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(bottom: 0),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: FlatButton(
+                      onPressed: null,
+                      child: new Text(
+                        "评论问答",
+                        maxLines: 1,
+                      ),
+                    )),
+                Expanded(
+                    child: FlatButton(
+                      onPressed: null,
+                      child: new Text(
+                        "企业视屏",
+                        maxLines: 1,
+                      ),
+                    )),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey))),
+            child: Container(
+              margin: EdgeInsets.all(3.0),
+              padding: EdgeInsets.all(0.0),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.circular(20)),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.search,),
+                    onPressed: null,
+                    iconSize: 30,),
+//                    TextField(
+//                      decoration: InputDecoration(labelText: 'Search..'),
+//                      style: TextStyle(fontSize: 20.0),
+//                    )
+
+                ],
+              ),
+            ), ),
+
+          Expanded(
+              child: ListView(
+
+                children: <Widget>[
+                  new Question(),
+                  new Question(),
+                  new Question(),
+                  new Question(),
+                  new Question(),
+                ],
+              ))
+        ],
+      ),
     );
   }
 }
+
