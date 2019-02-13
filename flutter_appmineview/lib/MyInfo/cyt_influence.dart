ef1dd3975869eff417ffec105c11879032bc37e9
cyt07 <1097574026@qq.com>
2019-02-13 17:46:56 -0500

    add cyt influnce; wenda; add material app

--- flutter_appmineview/lib/MyInfo/cyt_influence.dart
+++ flutter_appmineview/lib/MyInfo/cyt_influence.dart
@@ -9,45 +9,58 @@ class cyt_influence extends StatefulWidget {
 class _cyt_influenceState extends State<cyt_influence> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        bottom: TabBar(
-          tabs: [
-            Tab(child: Container(child: Text('影响力等级'),),),
-            Tab(child: Container(child: Text('如何提升影响力'),),),
-          ],
-        ),
-        title: new Container(
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  child: Container(
+                    child: Text('影响力等级'),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    child: Text('如何提升影响力'),
+                  ),
+                ),
+              ],
+            ),
+            title: new Container(
 //              color: Colors.grey,
-          decoration: new BoxDecoration(
-
-              color: Colors.grey
+              decoration: new BoxDecoration(color: Colors.grey
 
 //                borderRadius: new BorderRadius.circular(5.0)
-          ),
-          height: 100.0,
-          child: Column(children: <Widget>[
-            Column(children: <Widget>[
-              new Text("wode"),
-              new Text("wodeyingxiangli"),
+                  ),
+              height: 100.0,
+              child: Column(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      new Text("wode"),
+                      new Text("wodeyingxiangli"),
 //                  new Text("time")
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              //分别对应三个界面
+              Icon(Icons.person),
+              new ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context, idx) {
+                    return WenDa();
+                  }),
+              Icon(Icons.directions_bike),
             ],
-            )
-          ],
           ),
         ),
-
-      ),
-      body: TabBarView(
-        children: [                             //分别对应三个界面
-          Icon(Icons.person),
-          new ListView.builder(itemCount: 5,
-              itemBuilder: (context,idx){
-                return WenDa();
-              }),
-          Icon(Icons.directions_bike),
-        ],
       ),
     );
   }
