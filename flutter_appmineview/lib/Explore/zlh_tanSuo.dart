af093007dff2f0c566cdfe94dd2b8d9090076681
zhanglonghan <Zlhan2018!>
2019-02-16 18:11:34 -0500

    zlh 添加问答界面、热门短评界面 zlh 连接探索页面和提问

--- flutter_appmineview/lib/Explore/zlh_tanSuo.dart
+++ flutter_appmineview/lib/Explore/zlh_tanSuo.dart
@@ -1,41 +1,91 @@
 import 'package:flutter/material.dart';
 import 'remen.dart';
 import 'wenda.dart';
+import 'tiwen.dart';
 
-class tanSuo extends StatefulWidget {
+class TanSuo extends StatefulWidget {
   @override
-  _tanSuoState createState() => _tanSuoState();
+  _TanSuoState createState() => _TanSuoState();
 }
 
-class _tanSuoState extends State<tanSuo> {
+class _TanSuoState extends State<TanSuo> {
+  List<String> _floatBtnRText = ["提问", "提问", "视频"];
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
           appBar: AppBar(
             bottom: TabBar(
               tabs: [
-                Tab(child: Container(child: Text('问答'),),),
-                Tab(child: Container(child: Text('热门短评'),),),
-                Tab(child: Container(child: Text('视频'),),),
+                Tab(
+                  child: Container(
+                    child: Text('问答'),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    child: Text('热门短评'),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    child: Text('视频'),
+                  ),
+                ),
               ],
+              onTap: (val) {
+                setState(() {
+                  index = val;
+                });
+              },
             ),
             title: Text('Tabs Demo'),
           ),
           body: TabBarView(
-            children: [                             //分别对应三个界面
-              new ListView.builder(itemCount: 10,
-                  itemBuilder: (context,idx){
+            children: [
+              //分别对应三个界面
+              new ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, idx) {
                     return ReMen();
                   }),
-              new ListView.builder(itemCount: 10,
-                  itemBuilder: (context,idx){
+              new ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, idx) {
                     return WenDa();
                   }),
-              Icon(Icons.directions_bike),
+              new Icon(Icons.print),
             ],
+          ),
+          floatingActionButton: new FlatButton(
+            color: Colors.blue[300],
+            shape: StadiumBorder(),
+            child: Container(
+              color: Colors.transparent,
+              width: 65.0,
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.edit,color: Colors.white,),
+                  Text(
+                    _floatBtnRText[index],
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+            onPressed: () {
+              if (index == 0 || index == 1) {
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                      builder: (context) => new TiWen(),
+                    ));
+              }
+            },
           ),
         ),
       ),
@@ -43,3 +93,6 @@ class _tanSuoState extends State<tanSuo> {
   }
 }
 
+void _question() {
+  return null;
+}
--- flutter_appmineview/lib/ExplorePage.dart
+++ flutter_appmineview/lib/ExplorePage.dart
@@ -14,7 +14,7 @@ class ExplorePageState extends State<ExplorePage>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new tanSuo();
+    return new TanSuo();
   }
 }
 
